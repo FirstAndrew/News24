e6415f120c71f06182432b7de94236498fc0ff8a
dealer drug <andrey.antipov94@gmail.com>
2021-02-21 21:50:47 +0300

    Update (controllers) 21 02

---
+++ News24/Controllers/NewsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class NewsController : Controller
+{
+    public NewsController()
+    {
+
+    }
+
+    public IActionResult index()
+    {
+        return View();
+    }
+}
